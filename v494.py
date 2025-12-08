fa932d68ad12053172c879c0a0c908271ee03324
Shimamura Sakura <shimamura_sakura@pm.me>
2025-12-08 23:49:37 +0800

    example - use ycd

--- v494.py
+++ v494.py
@@ -1,15 +1,21 @@
+from yuri.fileformat import *
 import yuri.yuridec as yuridec
 import yuri.yuricom as yuricom
 
 # decompile into official YST syntax, compile with official tools
-yuridec.run('files/v494', 'example/v494', dcls=yuridec.YDecYuris)
+# use ycd file to recover system variable names
+with open('example/v494.ycd', 'rb') as fp:
+    yscd = YSCD.read(Rdr.from_bio(fp))
+    cdict = {v.name: (v.typ, i) for i, v in enumerate(yscd.vars)}
+yuridec.run('files/v494', 'example/v494', dcls=yuridec.YDecYuris, yscd=yscd)
 
 # decompile into my own YURI syntax (based on python ast module)
-yuridec.run('files/v494', 'example/v494', dcls=yuridec.YDecYuri)
+yuridec.run('files/v494', 'example/v494', dcls=yuridec.YDecYuri, yscd=yscd)
 
 # compile YURI syntax into YBN then YPF, enable parallelism (default ON)
+# use ycd to enable using original names for system variables
 yuricom.run(yuricom.KEY_290, 'example/v494', 494,
             'example/v494-work',  # folder to store intermediate results
             'files/v494',  # where original ysv.ybn, yse.ybn, yscfg.ybn go
             'example/v494.ypf',  # output file
-            mp_parallel=True)
+            cdict=cdict, mp_parallel=True)
